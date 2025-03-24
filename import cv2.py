615a6c1595459780577066fa13cf302aa7c96589
Bhanuka <bhanukawijerathne@gmail.com>
2025-03-24 11:18:19 +0530

    modified 16

--- import cv2.py
+++
@@ -1,8 +0,0 @@
-import cv2
-image_path = r"D:\UNI\OOP [pro]\CW\3Dimension\OPENCV\Photos\bp2.png"
-image = cv2.imread(image_path)
-
-if image is None:
-    print("Error: Image not found or cannot be loaded.")
-else:
-    print("Image loaded successfully!")
